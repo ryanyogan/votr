8b6d77cf046fbd9be20f2794aa60a3b98b3a9b06
Ryan Yogan <ryanyogan@gmail.com>
2014-03-08 20:07:21 -0600

    Fixed response bug

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ exports.index = function(req, res) {
 
 exports.voteSMS = function(req, res) {
   if (twilio.validateExpressRequest(req, config.twilio.key) || config.disableTwilioSigCheck) {
-    response.header('Content-Type', 'text/html');
+    res.header('Content-Type', 'text/html');
 
     var body = req.param('Body').trim();
 
@@ -25,30 +25,30 @@ exports.voteSMS = function(req, res) {
         // fail silent
         res.send('<Response></Response>');
       } else if (event.state == "off") {
-        response.send('<Response><Sms>Voting is now closed.</Sms></Response>');
+        res.send('<Response><Sms>Voting is now closed.</Sms></Response>');
       } else if (!utils.testint(body)) {
         console.log('Bad vote: ' + event.name + ', ' + from + ', ' + body);
-        response.send('<Response><Sms>Sorry, invalid vote.  Please text a number between 1 and ' + event.voteoptions.length + '</Sms></Response>');
+        res.send('<Response><Sms>Sorry, invalid vote.  Please text a number between 1 and ' + event.voteoptions.length + '</Sms></Response>');
       } else if (utils.testint(body) && (parseInt(bdy) <= 0 || parseInt(body) > event.voteoptions.length)) {
-        response.send('<Response><Sms>Sorry, invalid vote.  Please text a number between 1 and ' + event.voteoptions.length + '</Sms></Response>');
+        res.send('<Response><Sms>Sorry, invalid vote.  Please text a number between 1 and ' + event.voteoptions.length + '</Sms></Response>');
       } else if (events.hasVoted(event.from)) {
         console.log('Denying vote: ' + event.name + ', ' + from);
-        response.send('<Response><Sms>Sorry, you are only allowed to vote once.</Sms></Response>');
+        res.send('<Response><Sms>Sorry, you are only allowed to vote once.</Sms></Response>');
       } else {
         var vote = parseInt(body);
 
         events.saveVote(event, vote, from, function(err, res) {
           if (err) {
-            response.send('<Response><Sms>We encountered an error saving your vote. Try again?</Sms></Response>');
+            res.send('<Response><Sms>We encountered an error saving your vote. Try again?</Sms></Response>');
           } else {
             console.log('Accepted vote');
-            response.send('<Response><Sms>Thanks for your vote for ' + res.name + '. Powered by MooseHead.</Sms></Response>');
+            res.send('<Response><Sms>Thanks for your vote for ' + res.name + '. Powered by MooseHead.</Sms></Response>');
           }
         });
       }
     });
   } else {
-    response.statusCode = 403;
-    response.render('forbidden');
+    res.statusCode = 403;
+    res.render('forbidden');
   }
 };
